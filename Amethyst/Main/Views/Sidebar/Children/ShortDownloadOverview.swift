61e81fbbe0a7091b055c84877f6844dc34c09b84
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2025-02-09 17:16:49 +0100

    Test gpg key

--- Amethyst/Main/Views/Sidebar/Children/ShortDownloadOverview.swift
+++ Amethyst/Main/Views/Sidebar/Children/ShortDownloadOverview.swift
@@ -9,6 +9,6 @@ import SwiftUI
 
 struct ShortDownloadOverview: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        Text("Test")
     }
 }
